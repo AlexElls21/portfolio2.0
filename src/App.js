c259e7edf7b136cc3fce5b44bfc496c265bd28db
Ethan Wolz <ethan@creadiv.io>
2020-02-13 21:08:49 -0700

    Routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import styled from "styled-components";
 
 import Home from "./pages/Home";
+import About from "./pages/About";
 import { GlobalStyle } from "./theme";
 
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 55% 45%;
+
+  height: 100vh;
+  width: 100vw;
+`;
+
 const App = () => (
-  <div>
-    <GlobalStyle />
+  <GridContainer>
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
+      <GlobalStyle />
+      <div>
+        <Link to="/about">About</Link>
+        <Link to="/">Home</Link>
+      </div>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </div>
     </Router>
-  </div>
+  </GridContainer>
 );
 
 export default App;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,14 @@
-import React, { useState } from "react";
-import styled, { css } from "styled-components";
+import React from "react";
 import Prismic from "prismic-javascript";
 
 import usePrismic from "../../utils/usePrismic";
-
-const GridContainer = styled.div`
-  display: grid;
-  grid-template-columns: 55% 45%;
-
-  height: 100vh;
-  width: 100vw;
-
-  ${({ theme: { space } }) => css``}
-`;
 
 const Home = () => {
   const { loading } = usePrismic(Prismic.Predicates.at("document.type", "project"));
 
   if (loading) return <div>LOADING....</div>;
 
-  return (
-    <GridContainer canAccess>
-      <div>hi</div>
-      <div>bye</div>
-    </GridContainer>
-  );
+  return <div>hi</div>;
 };
 
 export default Home;
